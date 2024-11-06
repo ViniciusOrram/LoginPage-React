351d6304a1a890f03264d67b1f66133f2c381a77
Vinicius <vinicius.costa@orram.com.br>
2024-11-06 15:04:24 -0300

    feat: Add Books

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './pages/Login'
+import Book from './pages/Book'
 
 export default function AppRoutes(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact element={<Login/>}/>
-                {/* <Route path="/books"  element={<Books/>}/>
-                <Route path="/book/new/:bookId"  element={<NewBooks/>}/> */}
+                <Route path="/book"  element={<Book/>}/>
+                {/* <Route path="/book/new/:bookId"  element={<NewBooks/>}/> */}
             </Routes>
         </BrowserRouter>
     );
